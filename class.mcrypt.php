fb22e19bc97a459f05332877dba8c7aca4cc678a
Sak32009 <Sak32009@users.noreply.github.com>
2015-01-07 19:10:44 +0100

    changed author

--- class.mcrypt.php
+++ class.mcrypt.php
@@ -8,9 +8,9 @@
 	*
 	*	Project name:			mcrypt
 	*	Project url:			https://github.com/Sak32009/mcrypt
-	*	Author:					Sak32009 (skype: sak32009)
+	*	Author:					Sak32009 (http://sak32009.bl.ee)
 	*	Description:			Encrypt/Decrypt your data
-	*	Programming Language:	PHP5
+	*	Programming Language:	PHP5 >= 5.1.2
 	*	Version:				0.04final # 09/09/2014
 	*
 	*	Thanks to: Relyze, xSplit
